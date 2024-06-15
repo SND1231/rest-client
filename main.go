d2f12d0e9130c85e0caef7be23f18e83e78983df
Sanada.Y <villa7iniesta8@gmail.com>
2024-06-15 11:59:22 +0900

    add: Rest Clientを試せる環境を作成

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	r := getRouter()
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getRouter() chi.Router {
+	r := chi.NewRouter()
+	r.Get("/test", get)
+	r.With(auth).Post("/test", post)
+	r.With(auth).Post("/csv", uploadCSV)
+	return r
+}
+
+func auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		if auth != "ok_token" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get!!")
+	id := r.FormValue("id")
+	type response struct {
+		ID string `json:"id"`
+	}
+	res := response{ID: id}
+	http.Header.Add(w.Header(), "content-type", "application/json")
+	http.Header.Add(w.Header(), "Access-Control-Allow-Origin", "*")
+	v, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(v)
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	log.Println("Post!!")
+	id := r.PostFormValue("id")
+	name := r.PostFormValue("name")
+	type response struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	}
+	res := response{ID: id, Name: name}
+	http.Header.Add(w.Header(), "content-type", "application/json")
+	http.Header.Add(w.Header(), "Access-Control-Allow-Origin", "*")
+	v, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(v)
+}
+
+func uploadCSV(w http.ResponseWriter, r *http.Request) {
+	log.Println("CSV!!")
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	for i := 0; ; i++ {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			log.Println("EOF")
+			break
+		}
+		if err != nil {
+			log.Println("error", err)
+			break
+		}
+		log.Println(record)
+	}
+}
